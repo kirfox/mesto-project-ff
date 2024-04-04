9cbaae46c8bb3a81808a70fb25e7bb250ea6ef80
Kirill <muxin.kirill2011@yandex.ru>
2024-04-04 15:16:27 +0300

    fixed remarks 1

--- scripts/index.js
+++ scripts/index.js
@@ -5,25 +5,25 @@ const cardTemplate = document.querySelector("#card-template").content;
 const cardList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
-addCard = (name, link, deleteCard) => {
+const addCard = (card, deleteCard) => {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   const deleteBtn = cardElement.querySelector(".card__delete-button");
 
-  cardElement.querySelector(".card__title").textContent = name;
-  cardElement.querySelector(".card__image").src = link;
+  cardElement.querySelector(".card__title").textContent = card.name;
+  cardElement.querySelector(".card__image").src = card.link;
+  cardElement.querySelector(".card__image").alt ="На карточке изображено " + card.name;
 
-  deleteBtn.addEventListener("click", deleteCard);
+  deleteBtn.addEventListener("click", () => deleteCard(cardElement));
 
   return cardElement;
 };
 
 // @todo: Функция удаления карточки
-deleteCard = (e) => {
-  const listCard = e.target.closest(".card");
-  listCard.remove();
+const deleteCard = (cardElement) => {
+  cardElement.remove();
 };
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach((element) => {
-  cardList.append(addCard(element.name, element.link, deleteCard));
+  cardList.append(addCard(element, deleteCard));
 });
